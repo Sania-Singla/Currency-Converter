a9db3a13d4ce6060beec39dbd35a22811978000e
Sania Singla <singlasania7717@gmail.com>
2024-10-26 16:43:03 +0530

    small

--- currency.js
+++ currency.js
@@ -1,37 +1,32 @@
-const dropdowns=document.getElementsByClassName("dropdown");  //will return an array
-const submitBtn=document.getElementById("get-exchange-rate");
-const input=document.getElementById("input");
-const msg=document.getElementById("msg");
-const fromImage=document.getElementById("fromimage");
-const toImage=document.getElementById("toimage");
-const fromDropdown=document.getElementById("from-dropdown");
-const toDropdown=document.getElementById("to-dropdown");
+const dropdowns = document.getElementsByClassName("dropdown"); //will return an array
+const submitBtn = document.getElementById("get-exchange-rate");
+const input = document.getElementById("input");
+const msg = document.getElementById("msg");
+const fromImage = document.getElementById("fromimage");
+const toImage = document.getElementById("toimage");
+const fromDropdown = document.getElementById("from-dropdown");
+const toDropdown = document.getElementById("to-dropdown");
 
-window.addEventListener("load",function(){
-
-    input.value=1;
+window.addEventListener("load", function () {
+    input.value = 1;
     updateExchangeRate();
 });
 
-for(let j=0;j<dropdowns.length;j++)
-{
-    for(i in countryList)        // for-in loop is used for objects and returns the keys in 'i'  remember that i will be a string 
-    {
-        const newOption=document.createElement("option");
-        newOption.innerText=i;
-        newOption.setAttribute("value",i);
+for (let j = 0; j < dropdowns.length; j++) {
+    for (i in countryList) { // for-in loop is used for objects and returns the keys in 'i'  remember that i will be a string
+        const newOption = document.createElement("option");
+        newOption.innerText = i;
+        newOption.setAttribute("value", i);
         dropdowns[j].append(newOption);
-        if(i==="USD" &&j===0)
-        {                                       //to set the intial states on refresh
-            newOption.selected=true;   // or can use "...." becuase a non empty string is also a truthy value 
-        }
-        else if(i==="INR" &&j===1)
-        {
-            newOption.selected=true;
+        if (i === "USD" && j === 0) {
+            //to set the intial states on refresh
+            newOption.selected = true; // or can use "...." becuase a non empty string is also a truthy value
+        } else if (i === "INR" && j === 1) {
+            newOption.selected = true;
         }
     }
 
-    dropdowns[j].addEventListener("change",function(e){     
+    dropdowns[j].addEventListener("change", function (e) {
         updateFlag(e.target);
     });
 
@@ -45,15 +40,13 @@ for(let j=0;j<dropdowns.length;j++)
 //     updateFlag(e.target);
 // });
 
+function updateFlag(target) {
+    let currencyCode = target.value;
+    let countryCode = countryList[currencyCode];
+    let image = target.parentElement.querySelector("img");
+    image.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
 
-function updateFlag(target)
-{
-    let currencyCode=target.value;
-    let countryCode=countryList[currencyCode];
-    let image=target.parentElement.querySelector("img");
-    image.src=`https://flagsapi.com/${countryCode}/flat/64.png`;
-
-    //or 
+    //or
     // if(target.name==="from")
     // {
     //     fromImage.src=`https://flagsapi.com/${countryCode}/flat/64.png`;
@@ -64,35 +57,31 @@ function updateFlag(target)
     // }
 }
 
-
-submitBtn.addEventListener("click",async function(e){
-
+submitBtn.addEventListener("click", async function (e) {
     e.preventDefault();
     updateExchangeRate();
 });
 
+async function updateExchangeRate() {
+    let fromCurrencyCode = fromDropdown.value;
+    let fromCountryCode = countryList[fromCurrencyCode];
+    let toCurrencyCode = toDropdown.value;
+    let toCountryCode = countryList[toCurrencyCode];
 
-async function updateExchangeRate(){
-
-    let fromCurrencyCode=fromDropdown.value;
-    let fromCountryCode=countryList[fromCurrencyCode];
-    let toCurrencyCode=toDropdown.value;
-    let toCountryCode=countryList[toCurrencyCode];
-
-    let URL=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrencyCode.toLowerCase()}.json`;
-    let response=await fetch(URL);
-    let redable=await response.json();
+    let URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrencyCode.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let redable = await response.json();
     // console.log(redable[fromCurrencyCode.toLowerCase()]);
-    let baseCurrencyCode=redable[fromCurrencyCode.toLowerCase()];
-    let exchangeRate=baseCurrencyCode[toCurrencyCode.toLowerCase()];
+    let baseCurrencyCode = redable[fromCurrencyCode.toLowerCase()];
+    let exchangeRate = baseCurrencyCode[toCurrencyCode.toLowerCase()];
     // console.log(exchangeRate);
     // console.log(baseCurrencyCode[toCurrencyCode.toLowerCase()]);
 
-    if((input.value<0)||(input.value==="")) 
-    {
+    if (input.value < 0 || input.value === "") {
         alert("please enter a valid amount!");
-        input.value=1;      // or "1" will also work
+        input.value = 1; // or "1" will also work
     }
-    msg.innerText=`${input.value} ${fromCurrencyCode} = ${exchangeRate*(input.value)} ${toCurrencyCode}`;
-
+    msg.innerText = `${input.value} ${fromCurrencyCode} = ${
+        exchangeRate * input.value
+    } ${toCurrencyCode}`;
 }
